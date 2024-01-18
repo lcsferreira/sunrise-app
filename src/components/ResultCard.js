55a457a597165ed8e79ad06ca74cf9d54b4ab99c
Lucas Ferreira <lucas.simoes.ferreira@gmail.com>
2024-01-18 18:39:57 -0300

    FEAT: Carousel with the information in the result-card

--- src/components/ResultCard.js
+++ src/components/ResultCard.js
@@ -1,24 +1,93 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+
 import "./index.css";
+import Day from "../assets/day.svg";
+import Sunrise from "../assets/sunrise.svg";
+import Sunset from "../assets/sunset.svg";
 
 const ResultCard = ({ results }) => {
+  //create an array with the sunrise time, sunset time, solar noon time, day length, golden hour, dusk, dawn and last light
+  const times = [
+    {
+      id: 1,
+      name: "Begin",
+      // info: [results?.sunrise, results?.dawn],
+      info: [
+        { title: "Sunrise", time: results?.sunrise },
+        { title: "Dawn", time: results?.dawn },
+      ],
+      icon: Sunrise,
+    },
+    {
+      id: 2,
+      name: "Mid",
+      // info: [results?.day_length, results?.golden_hour, results?.solar_noon],
+      info: [
+        { title: "Day Length", time: results?.day_length },
+        { title: "Golden Hour", time: results?.golden_hour },
+        { title: "Solar Noon", time: results?.solar_noon },
+      ],
+      icon: Day,
+    },
+    {
+      id: 3,
+      name: "End",
+      info: [
+        { title: "Sunset", time: results?.sunset },
+        { title: "Dusk", time: results?.dusk },
+        { title: "Last Light", time: results?.last_light },
+      ],
+      icon: Sunset,
+    },
+  ];
+
+  const formattedDate = (date) => {
+    const d = new Date(date);
+    return d.toLocaleDateString();
+  };
+
+  const formattedTime = (time) => {
+    let hour = time?.split(":")[0];
+    //make the hour double digit
+    if (hour?.length === 1) {
+      hour = "0" + hour;
+    }
+    const minutes = time?.split(":")[1];
+    return `${hour}:${minutes}`;
+  };
+
   return (
     <div className="result-card">
-      <h2>Information about the Sun</h2>
-      <div>
-        <h3>{results?.date}</h3>
-        <p>Sunrise: {results?.sunrise}</p>
-        <p>Sunset: {results?.sunset}</p>
-        <p>First Light: {results?.first_light}</p>
-        <p>Last Light: {results?.last_light}</p>
-        <p>Dawn: {results?.dawn}</p>
-        <p>Dusk: {results?.dusk}</p>
-        <p>Solar Noon: {results?.solar_noon}</p>
-        <p>Golden Hour: {results?.golden_hour}</p>
-        <p>Day Length: {results?.day_length}</p>
-        <p>Timezone: {results?.timezone}</p>
-        <p>UTC Offset: {results?.utc_offset}</p>
-      </div>
+      <h2>{formattedDate(results?.date)}</h2>
+      <Carousel
+        // autoPlay
+        infiniteLoop
+        centerMode
+        interval={4000}
+        showArrows={true}
+        showStatus={false}
+        showIndicators={false}
+        showThumbs={false}
+        axis="horizontal"
+        centerSlidePercentage={100}
+      >
+        {times.map((time) => (
+          <div key={time.id} className="carousel-div">
+            <h3>{time.name}</h3>
+            <img key={time.id} src={time.icon} alt={time.name} />
+            <ul>
+              {time.info.map((info) => (
+                <li key={info.title}>
+                  <h3>{info.title}</h3>
+                  <p>{formattedTime(info.time)}</p>
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </Carousel>
     </div>
   );
 };
